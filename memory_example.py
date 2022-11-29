e5dd9a634c933d2daa99d1bd134a8a8e40c73cb0
oritzau <oritzau2021@gmail.com>
2022-11-29 15:02:58 -0500

    Updated python example

--- memory_example.py
+++ memory_example.py
@@ -2,9 +2,11 @@
 str = "Hello World"
 
 # What your computer sees:
-0x1f94753b8b0 = str = "Hello World" #Don't let this crazy string of numbers and letters spook you!
+str = "Hello World" 
+str_address = 0x1f94753b8b0 #Don't let this crazy string of numbers and letters spook you!
 #The computer recognizes the line above as an address in the physical memory of your computer
 #now that address has a value "Hello World", and can be used elsewhere
-print(str) #we use our variable x for the last time here
+
+print(str) #let's say we use our variable x for the last time here
 #at this point, the garbage collector sweeps through and cleans up the memory we allocated to str
 #str no longer exists in the physical memory of your computer
