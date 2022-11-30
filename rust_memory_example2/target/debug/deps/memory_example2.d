b16fd4b81ff4c844a3a8e3775d60d502ff2f1fb3
oritzau <oritzau2021@gmail.com>
2022-11-29 19:08:37 -0500

    Fixed extraneous file

--- rust_memory_example2/target/debug/deps/memory_example2.d
+++ rust_memory_example2/target/debug/deps/memory_example2.d
@@ -1,5 +1,5 @@
-C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\memory_example2\target\debug\deps\memory_example2.exe: src\main.rs
+C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\rust_memory_example2\target\debug\deps\memory_example2.exe: src\main.rs
 
-C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\memory_example2\target\debug\deps\memory_example2.d: src\main.rs
+C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\rust_memory_example2\target\debug\deps\memory_example2.d: src\main.rs
 
 src\main.rs:
