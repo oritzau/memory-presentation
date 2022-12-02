a150f39e9bec0c9cd2d746c4995a7451b7f892e7
oritzau <oritzau2021@gmail.com>
2022-12-01 23:10:41 -0500

    Fixed small error in rust example 1

--- rust_memory_example1/target/debug/deps/memory_example.d
+++ rust_memory_example1/target/debug/deps/memory_example.d
@@ -1,5 +1,5 @@
-C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\memory_example\target\debug\deps\memory_example.exe: src\main.rs
+C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\rust_memory_example1\target\debug\deps\memory_example.exe: src\main.rs
 
-C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\memory_example\target\debug\deps\memory_example.d: src\main.rs
+C:\Users\owen\Documents\cs106\vscode\git\memory-presentation\rust_memory_example1\target\debug\deps\memory_example.d: src\main.rs
 
 src\main.rs:
